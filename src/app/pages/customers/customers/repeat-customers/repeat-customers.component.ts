66082222a4bed6bf2ae676bc40595b93b0438d36
Robert Calero <rmorck03@gmail.com>
2022-12-04 11:11:57 -0600

    s

---
+++ src/app/pages/customers/customers/repeat-customers/repeat-customers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repeat-customers',
+  templateUrl: './repeat-customers.component.html',
+  styleUrls: ['./repeat-customers.component.scss']
+})
+export class RepeatCustomersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
