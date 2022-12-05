3a9818739727ce884896424e57f7a9faac762784
jerryMelendez <mjerrymoises@gmail.com>
2022-12-04 23:12:42 -0600

    new settings

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/@core/services/users.service';
 
 @Component({
   selector: 'app-settings',
@@ -8,12 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class SettingsComponent implements OnInit {
 
   public identity: any;
-  constructor() { }
+  public token: string = '';
+  public users: any[] = [];
+  constructor(
+    private userService: UsersService
+  ) { }
 
   ngOnInit(): void {
     let data = localStorage.getItem('identity') || '{}';
     this.identity = JSON.parse(data);
-    console.log(this.identity);
+    let token = localStorage.getItem('token') || '';
+    this.token = token;
+    if (this.token){
+      this.getUsers();
+    }
+  }
+
+  async getUsers(){
+    let resp = await this.userService.getAllStaffByBusiness(this.identity.businessList[0].code);
+    console.log(resp);
+    if (resp.status === '200' && resp.data){
+      this.users = resp.data;
+    }
   }
 
 }
