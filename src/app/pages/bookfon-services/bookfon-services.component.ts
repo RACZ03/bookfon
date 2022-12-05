12af9e6e5bd82c716f6c6bc78ef1d3ecb7d2f3a0
jerryMelendez <mjerrymoises@gmail.com>
2022-12-04 20:03:29 -0600

    bookfon services

---
+++ src/app/pages/bookfon-services/bookfon-services.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookfon-services',
+  templateUrl: './bookfon-services.component.html',
+  styleUrls: ['./bookfon-services.component.scss']
+})
+export class BookfonServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
