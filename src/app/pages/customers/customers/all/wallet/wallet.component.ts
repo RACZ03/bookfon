379492bd8b360b8ff3b1160cdd5648a6d750b4b2
Robert Calero <rmorck03@gmail.com>
2022-12-05 23:34:28 -0600

    Module sub-customers and wallet %80

--- src/app/pages/customers/customers/all/wallet/wallet.component.ts
+++ src/app/pages/customers/customers/all/wallet/wallet.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataTableDirective } from 'angular-datatables';
+import { Subject } from 'rxjs';
+import { CustomersService } from 'src/app/@core/services/customers.service';
+import { TransactionsService } from 'src/app/@core/services/transactions.service';
+import { AlertService } from 'src/app/@core/utils/alert.service';
+
+declare var window: any;
 
 @Component({
   selector: 'app-wallet',
@@ -7,9 +15,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(DataTableDirective, {static: false})
+  dtElement!: DataTableDirective;
 
-  ngOnInit(): void {
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+  public formModalRecharge: any;
+  public data: any[] = [];
+  public idSelected: number = 0;
+  public balanceObj: any;
+
+  constructor(
+    private customerSvc: CustomersService,
+    private alertSvc: AlertService,
+    private route: ActivatedRoute,
+    private TransactionSvc: TransactionsService
+  ) { 
+    this.dtOptions = {
+      // pagingType: 'full_numbers',
+      pagingType: "simple_numbers",
+      pageLength: 5,
+      scrollX: true,
+      autoWidth: false,
+      destroy: true,
+      responsive: true,
+      dom: 'Bfrtip',
+      searching: true,
+      search: false,
+      info: false,
+    }
   }
 
+  ngOnInit(): void {
+
+    this.formModalRecharge = new window.bootstrap.Modal(
+      document.getElementById('modalRecharge')
+    );
+    // get id from url
+    this.route.params.subscribe(params => {
+      this.idSelected = params['id'];
+      this.loadDataBalance();
+      this.loadDataTransactions();
+    });
+
+  }
+
+  searchData(e: any) {
+    let value = e.target.value;
+    this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+      dtInstance.search(value).draw();
+    });
+  }
+
+
+  async loadDataBalance() {
+    let resp = await this.TransactionSvc.getBalance(this.idSelected);
+    // console.log(resp);
+    if ( resp?. status == 200 ) {
+      let { data } = resp;
+      if ( data !== undefined ) {
+        this.balanceObj = data;
+      }
+    } else {
+      this.alertSvc.showAlert(3,'', 'No data found');
+    }
+
+    // this.dtTrigger.next(this.dtOptions);
+  }
+
+  async loadDataTransactions() {
+    let resp = await this.TransactionSvc.getTransacciones(this.idSelected);
+    console.log(resp);
+    if ( resp?. status == 200 ) {
+      let { data } = resp;
+      if ( data !== undefined ) {
+        this.data = data;
+      }
+    } else {
+      this.alertSvc.showAlert(3,'', 'No data found');
+    }
+
+    this.dtTrigger.next(this.dtOptions);
+  }
+
+  /*  OPEN MODAL RECHARGE  */
+  openModalRecharge() {
+    this.formModalRecharge.show();
+  }
 }
