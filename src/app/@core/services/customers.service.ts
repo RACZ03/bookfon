36bfd06ce4f7e692d707d4c9879a63c0c7d7fdc1
Robert Calero <rmorck03@gmail.com>
2022-12-07 23:55:46 -0600

    Add schedule customers

--- src/app/@core/services/customers.service.ts
+++ src/app/@core/services/customers.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
 import { ConnectionService } from '../utils/connection.service';
 
 // import { AngularFireStorage } from '@angular/fire/compat/storage';
@@ -34,6 +35,46 @@ export class CustomersService {
 
   async getSchedule(id: number) {
     let code = this.getCode();
-    return this.connectionSvc.send('get', `v1/purchasedService/${code}/customer/${ id }`);
+    let data = await this.connectionSvc.send('get', `v1/purchasedService/${code}/customer/${ id }`);
+
+    if ( data == null || data.data == null ) { 
+      return [];
+    }
+
+    const scheduled = this.processDataSchedule(data.data) || [];
+    return scheduled;
   }
+
+  processDataSchedule(data: any) {
+    let events = [];
+
+    for (let index = 0; index < data.length; index++) {
+      events.push({
+        id: data[index].id,
+        title: data[index]['service'].name,
+        start: this.convertDate(data[index].date),
+        end: this.convertDate(data[index].date),
+        time: {
+          startTime: data[index].startTime,
+          endTime: data[index].endTime,
+        },
+        service: data[index]['service'],
+        service_name: data[index]['service'].name,
+        service_description: data[index]['service'].description,
+        allDay: false,
+        staff: data[index]['staff'],
+        url: data[index]['staff'].image? data[index]['staff'].image : '',
+        backgroundColor: '#FF946F',
+        borderColor: '#FF946F',
+        textColor: '#000',
+        description: data[index]['service'].description,
+      });
+    }
+    return events;
+  }
+
+  convertDate(date: any) {
+    return moment(date).format('YYYY-MM-DD');
+  }
+
 }
