6a068438cbe7fbc119f99bb6ba15e4c3795b920b
Robert Calero <rmorck03@gmail.com>
2022-12-09 17:53:58 -0600

    Update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // APIUrl: 'https://schema.postman.com/json/collection/v2.1.0', // <-- URL Production
-  APIUrl: 'https://api.bookfon.com', // <-- URL Development
+  APIUrl: 'https://api.bookfon.com', // <-- URL Production
+  //APIUrl: 'http://bookfonstag.us-east-1.elasticbeanstalk.com', // <-- URL Local
   firebaseConfig: {
     apiKey: "AIzaSyAL81QAR_z-odJJ4cNgJuPozc2H0gWPa7w",
     authDomain: "bpb-training.firebaseapp.com",
