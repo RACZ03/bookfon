b646ba47c6e246ae74086bdcbbe57e304ec71042
Robert Calero <rmorck03@gmail.com>
2023-01-09 12:23:38 -0600

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // APIUrl: 'https://api.bookfon.com', // <-- URL Production
-  APIUrl: 'https://testbookfon.bookfon.com', // <-- URL development
+  APIUrl: 'https://api.bookfon.com', // <-- URL Production
+  // APIUrl: 'https://testbookfon.bookfon.com', // <-- URL development
   // APIUrl: 'http://bookfonstag.us-east-1.elasticbeanstalk.com', // <-- URL Local
   firebaseConfig: {
     apiKey: "AIzaSyAL81QAR_z-odJJ4cNgJuPozc2H0gWPa7w",
